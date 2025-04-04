23980137feb859756e07790bd86d37346868987e
panteLx <sebi.stahl1811@live.com>
2025-04-04 18:57:37 +0200

    x

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-source ~/purge-ftp/bin/activate
-python3 ~/purge-ftp/purge.py
+cd ~/ftp-purge
+python3 -m venv ftp-purge-venv
+source ftp-purge-venv/bin/activate
+pip install -r requirements.txt
+python3 purge.py
